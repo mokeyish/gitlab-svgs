e5b41c91fc257a72f6b63e47c60959be808a5830
Lukas Eipert <leipert@gitlab.com>
2020-02-05 23:39:56 +0100

    Fix lints

--- build_scripts/src/illustrations.js
+++ build_scripts/src/illustrations.js
@@ -1,13 +1,12 @@
-const async = require('async');
 const SVGO = require('svgo');
 const path = require('path');
 const mkdirp = require('mkdirp');
 const glob = require('glob');
 
-const utils = require('./utils');
+const { getFilesizeInBytes, readFilePromise, writeFilePromise } = require('./utils');
 
 module.exports = {
-  optimizeIllustrations: (BASE_PATH, finishedCallback) => {
+  optimizeIllustrations: async (BASE_PATH, finishedCallback) => {
     const illustrationFilesPath = path.join(BASE_PATH, 'illustrations');
     const dest = path.normalize(path.join(BASE_PATH, 'dist'));
     const illustrationFiles = glob.sync(`${illustrationFilesPath}/**/*.svg`, {});
@@ -19,58 +18,49 @@ module.exports = {
       ],
     });
 
-    const illustrations = [];
-
     console.log(`Optimize ${illustrationFiles.length} Illustrations`);
 
-    async.forEachOf(
-      illustrationFiles,
-      (file, key, callback) => {
-        const relName = path.relative(BASE_PATH, file);
-        const fpath = path.join(dest, relName);
+    const optimizeIllustration = async file => {
+      const relName = path.relative(BASE_PATH, file);
+      const fpath = path.join(dest, relName);
 
-        utils
-          .readFilePromise(path.resolve(file), 'utf8')
-          .then(data => svgo.optimize(data, { path: path.resolve(file) }))
-          .then(result => {
-            mkdirp.sync(path.dirname(fpath));
+      const illustration = await readFilePromise(path.resolve(file), 'utf8');
 
-            return utils.writeFilePromise(fpath, result.data);
-          })
-          .then(() => {
-            illustrations.push({
-              name: relName,
-              size: utils.getFilesizeInBytes(fpath),
-            });
-            callback();
-          })
-          .catch(e => callback(e));
-      },
-      err => {
-        if (err) {
-          return finishedCallback(err);
-        }
+      const optimizedIllustration = await svgo.optimize(illustration, { path: path.resolve(file) });
 
-        // Save the Illustrations Info to a JSON
-        const illustrationsInfo = {
-          illustrationCount: illustrations.length,
-          illustrations: illustrations.sort((a, b) => {
-            if (a.name === b.name) {
-              return 0;
-            }
-            return a.name < b.name ? -1 : 1;
-          }),
-        };
+      mkdirp.sync(path.dirname(fpath));
 
-        return utils
-          .writeFilePromise(
-            path.join(dest, 'illustrations.json'),
-            JSON.stringify(illustrationsInfo, null, 2),
-            'utf8',
-          )
-          .then(() => finishedCallback())
-          .catch(e => finishedCallback(e));
-      },
-    );
+      await writeFilePromise(fpath, optimizedIllustration.data);
+
+      return {
+        name: relName,
+        size: getFilesizeInBytes(fpath),
+      };
+    };
+
+    try {
+      const illustrations = await Promise.all(illustrationFiles.map(optimizeIllustration));
+
+      // Save the Illustrations Info to a JSON
+      const illustrationsInfo = {
+        illustrationCount: illustrations.length,
+        illustrations: illustrations.sort((a, b) => {
+          if (a.name === b.name) {
+            return 0;
+          }
+          return a.name < b.name ? -1 : 1;
+        }),
+      };
+
+      await writeFilePromise(
+        path.join(dest, 'illustrations.json'),
+        JSON.stringify(illustrationsInfo, null, 2),
+        'utf8',
+      );
+
+      finishedCallback();
+    } catch (e) {
+      finishedCallback(e);
+    }
   },
 };
