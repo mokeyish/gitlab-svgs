689d2437cb1545035392e49bd39e2f8a986acf76
Lukas Eipert <leipert@gitlab.com>
2021-04-22 23:16:01 +0200

    Remove deprecated eslint-loader
    See: https://gitlab.com/gitlab-org/gitlab-services/design.gitlab.com/-/merge_requests/2321

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,19 +50,5 @@ module.exports = {
   /*
    ** Build configuration
    */
-  build: {
-    /*
-     ** Run ESLint on save
-     */
-    extend(config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /node_modules/,
-        });
-      }
-    },
-  },
+  build: {},
 };
