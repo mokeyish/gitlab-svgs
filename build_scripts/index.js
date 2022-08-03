30e78d81362a0b2e48919f785ed5d4ad7a53de91
Lukas Eipert <leipert@gitlab.com>
2022-08-03 11:43:45 +0200

    Add some review metadata

--- build_scripts/index.js
+++ build_scripts/index.js
@@ -1,16 +1,50 @@
+const { writeFile, mkdir } = require('fs/promises');
 const path = require('path');
 const rimraf = require('rimraf');
 
+const packageJSON = require('../package.json');
 const { optimizeSVGs } = require('./src/svg_optimization');
 const { createIconSprite } = require('./src/svg_sprite_icons');
 const { copyFolderRecursive, copyFile } = require('./src/utils');
 const { collectIllustrations } = require('./src/illustrations');
 
 const BASE_PATH = path.join(__dirname, '..');
+const REVIEW_PATH = path.join(BASE_PATH, 'review');
 
 const DIST_PATH = path.join(BASE_PATH, 'dist');
 const FILE_ICONS_DIST_PATH = path.join(DIST_PATH, 'file_icons');
 const STATIC_PATH = path.normalize(path.join(BASE_PATH, 'svgpreviewer', 'static'));
+
+async function writeReviewMetadata() {
+  await mkdir(REVIEW_PATH, { recursive: true });
+
+  let data = {
+    version: packageJSON.version,
+  };
+
+  if (process.env.CI) {
+    const {
+      CI_JOB_ID,
+      CI_PIPELINE_URL,
+      CI_MERGE_REQUEST_PROJECT_URL,
+      CI_MERGE_REQUEST_IID,
+      CI_MERGE_REQUEST_TITLE,
+    } = process.env;
+
+    data = {
+      ...data,
+      jobId: CI_JOB_ID,
+      pipelineURL: CI_PIPELINE_URL,
+    };
+
+    if (CI_MERGE_REQUEST_PROJECT_URL && CI_MERGE_REQUEST_IID) {
+      data.mrURL = `${CI_MERGE_REQUEST_PROJECT_URL}/-/merge_requests/${CI_MERGE_REQUEST_IID}`;
+      data.mrTitle = CI_MERGE_REQUEST_TITLE;
+    }
+  }
+
+  await writeFile(path.join(REVIEW_PATH, 'metadata.json'), JSON.stringify(data));
+}
 
 async function buildFiles() {
   console.log('Creating Icon Sprite...');
@@ -56,6 +90,9 @@ async function buildFiles() {
 
   console.log('Copying file_icons notice');
   await copyFile(path.join(BASE_PATH, 'file_icons', 'LICENSE.md'), FILE_ICONS_DIST_PATH);
+
+  console.log('Writing out metadata');
+  await writeReviewMetadata();
 }
 
 rimraf(`${DIST_PATH}/**/*`, async () => {
